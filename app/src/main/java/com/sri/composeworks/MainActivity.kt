a8972de4e09013176710cf9685b393f0d7ad8381
srikanth4mobileapps <srikanth4mobileapps@gmail.com>
2023-12-12 23:54:33 +0800

    Modifier added context in composable added

--- app/src/main/java/com/sri/composeworks/MainActivity.kt
+++ app/src/main/java/com/sri/composeworks/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.sri.composeworks
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sri.composeworks.ui.theme.ComposeWorkTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,21 +46,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
+    val activity = LocalContext.current as Activity
+
+    Column(modifier = Modifier
+        .fillMaxSize(1f)
+        .background(Color.Green)
+        .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = "Let's start working together with $name!",
-            modifier = modifier
+            modifier = Modifier
+                .border(5.dp, Color.Yellow)
+                .padding(20.dp)
+
         )
         Text(
             text = "Yes $name!",
-            modifier = modifier
+            modifier = Modifier
+                .padding(20.dp)
+                .border(5.dp, Color.Black)
+                .padding(20.dp)
+                .clickable {
+                    Toast.makeText(activity, "Ok Fine", Toast.LENGTH_SHORT)
+                        .show()
+                }
         )
         Text(
             text = "No $name!",
-            modifier = modifier
+            modifier = Modifier
+                .padding(20.dp)
+                .border(5.dp, Color.Red)
+                .padding(20.dp)
+                .clickable {
+                    Toast.makeText(activity, "Duck you", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
         )
     }
 }
